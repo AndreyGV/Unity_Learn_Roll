4aa8dfadc10f4dadbd84330d4070d5da21b31e6e
AndreyGV <74981070+AndreyGV@users.noreply.github.com>
2023-03-29 23:34:38 +0300

    Update PlayerController.cs

--- UnityStudyProject4Final/Assets/Scripts/PlayerController.cs
+++ UnityStudyProject4Final/Assets/Scripts/PlayerController.cs
@@ -12,14 +12,13 @@ public class PlayerController : MonoBehaviour
     private float powerupStrength = 15.0f;
     private Rigidbody playerRb;
     private GameObject focalPoint;
-    // Start is called before the first frame update
+   
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         focalPoint = GameObject.Find("Focal Point");
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
